6608f85f5de2ea66527b9fbb32a8cf693df41ca5
Bryson McKenzie <brysonmckenzie83@gmail.com>
2017-08-14 13:35:25 -0500

    Update to assignments

---
+++ Angular/US-Timezone-Display/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  time = new Date();
+  lastTimeZoneSelected = null;
+
+  getDateByZone(timezone) {
+    this.time = new Date();
+    if (timezone === 'MST') {
+      this.time.setHours(this.time.getHours() - 1);
+    } else if (timezone === 'CST') {
+      this.time.setHours(this.time.getHours());
+    } else if (timezone === 'EST') {
+      this.time.setHours(this.time.getHours() + 1);
+    } else if (timezone === 'PST') {
+      this.time.setHours(this.time.getHours() - 2);
+    }
+    this.lastTimeZoneSelected = timezone;
+  }
+
+}
